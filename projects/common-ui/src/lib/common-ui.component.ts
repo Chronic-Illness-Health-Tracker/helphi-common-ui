48e85bde1efe987183dc8e16aee1a2b577896aaf
JamieTremaine <jamie.tremaine@students.plymouth.ac.uk>
2024-02-08 18:51:32 +0000

    init project

---
+++ projects/common-ui/src/lib/common-ui.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-common-ui',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      common-ui works!
+    </p>
+  `,
+  styles: ``
+})
+export class CommonUiComponent {
+
+}
