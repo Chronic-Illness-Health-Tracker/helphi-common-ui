00b1b1390b655c44ce77cfcfe457b4bf946c4fd3
JamieTremaine <jamie.tremaine@students.plymouth.ac.uk>
2024-02-19 03:21:37 +0000

    chips

--- projects/helphi-common-ui/src/lib/components/helphi-chip/helphi-chip.component.ts
+++ projects/helphi-common-ui/src/lib/components/helphi-chip/helphi-chip.component.ts
@@ -1,13 +1,13 @@
+import { NgClass } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
     selector: 'helphi-chip',
     standalone: true,
-    imports: [],
+    imports: [NgClass],
     templateUrl: './helphi-chip.component.html',
     styleUrl: './helphi-chip.component.scss',
 })
 export class HelphiChipComponent {
-    @Input({ required: true }) content!: string;
     @Output() closed = new EventEmitter<null>();
 }
